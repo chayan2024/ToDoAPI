280d1514d1c1903dd4dac7277357ba3ac18772c8
chayan <chayanchowdhury@gmail.com>
2024-06-11 13:08:19 +0530

    Login issue resolved

--- src/main/java/org/example/todoapi/config/SpringSecurityConfig.java
+++ src/main/java/org/example/todoapi/config/SpringSecurityConfig.java
@@ -1,7 +1,9 @@
 package org.example.todoapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,6 +21,11 @@ public class SpringSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
